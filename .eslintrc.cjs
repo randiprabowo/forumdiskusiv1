957021693cb0e9d2d7595db81a240769f2c861c2
unknown <randiprabowo5@gmail.com>
2025-09-13 11:32:25 +0700

    fix: update eslint configuration and dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,11 +2,14 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
+    node: true,
   },
   extends: [
-    'airbnb',
+    'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'airbnb',
   ],
   parserOptions: {
     ecmaFeatures: {
@@ -15,21 +18,61 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   plugins: [
     'react',
     'react-hooks',
     'react-refresh',
-    'import',
-    'jsx-a11y',
   ],
   rules: {
-    'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-    'react/react-in-jsx-scope': 'off', // Not needed in React 17+
-    'react/prop-types': 'off', // We'll use prop validation in a different way
-    'import/prefer-default-export': 'off',
-    'react/jsx-filename-extension': [1, { extensions: ['.jsx'] }],
-    'no-param-reassign': ['error', { props: false }], // Allow reassigning properties of parameters
-    'no-console': 'warn', // Change console errors to warnings
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'no-param-reassign': ['error', { props: true, ignorePropertyModificationsFor: ['state'] }],
+    'no-underscore-dangle': 'off',
+    'linebreak-style': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never'
+      }
+    ]
   },
-  ignorePatterns: ['dist/**/*'],
+  ignorePatterns: [
+    'dist/**/*',
+    'build/**/*',
+    'coverage/**/*',
+    'node_modules/**/*'
+  ],
+  overrides: [
+    {
+      files: [
+        '**/*.test.js',
+        '**/*.test.jsx',
+        '**/*.spec.js',
+        '**/*.spec.jsx',
+        'cypress/**/*.js'
+      ],
+      env: {
+        jest: true,
+        'cypress/globals': true
+      },
+      plugins: ['jest', 'cypress'],
+      rules: {
+        'jest/no-disabled-tests': 'warn',
+        'jest/no-focused-tests': 'error',
+        'jest/no-identical-title': 'error',
+        'jest/prefer-to-have-length': 'warn',
+        'jest/valid-expect': 'error'
+      }
+    }
+  ]
 }
