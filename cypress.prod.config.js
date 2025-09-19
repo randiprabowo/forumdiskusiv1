a5d6780381f0efe7c572978905104437e7b90bd8
unknown <randiprabowo5@gmail.com>
2025-09-19 09:20:40 +0700

    Update E2E test configuration for production

---
+++ cypress.prod.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    supportFile: 'cypress/support/e2e.js',
+    video: false,
+    defaultCommandTimeout: 10000,
+    requestTimeout: 10000,
+    responseTimeout: 10000,
+    pageLoadTimeout: 30000,
+    retries: {
+      runMode: 2,
+      openMode: 0
+    },
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
